ba585d6bb59f7e4884ee8192813107b7b14ff3c5
Matthew Faraz Tootoonchi <44936371+mtootoonchi@users.noreply.github.com>
2022-08-19 08:15:36 -0700

    Add files via upload

---
+++ Automated Flight Program with Barcode Search Test #1/code/get_lines_python_demo.py
@@ -0,0 +1,217 @@
+from __future__ import print_function
+import pixy
+from ctypes import *
+from pixy import *
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+from pymavlink import mavutil # Needed for command message definitions
+import time
+import math
+
+# Set up option parsing to get connection string
+import argparse
+parser = argparse.ArgumentParser(description='Control Copter and send commands in GUIDED mode ')
+parser.add_argument('--connect',
+                   help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+sitl = None
+
+# Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+
+# Connect to the Vehicle
+print('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
+
+def arm():
+    print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+    # If you need to disable the arming check,
+    # just comment it with your own responsibility.
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+def guided():
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        vehicle.armed = True
+        time.sleep(1)
+
+def guided_nogps():
+    print("Arming motors")
+    # Copter should arm in GUIDED_NOGPS mode
+    vehicle.mode = VehicleMode("GUIDED_NOGPS")
+    vehicle.armed = True
+
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        vehicle.armed = True
+        time.sleep(1)
+
+def takeoff_nogps(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude without GPS data.
+    """
+
+    ##### CONSTANTS #####
+    DEFAULT_TAKEOFF_THRUST = 0.7
+    SMOOTH_TAKEOFF_THRUST = 0.6
+
+    print("Taking off!")
+
+    thrust = DEFAULT_TAKEOFF_THRUST
+    while True:
+        current_altitude = vehicle.location.global_relative_frame.alt
+        print(" Altitude: %f  Desired: %f" %
+              (current_altitude, aTargetAltitude))
+        if current_altitude >= aTargetAltitude*0.95: # Trigger just below target alt.
+            print("Reached target altitude")
+            break
+        elif current_altitude >= aTargetAltitude*0.6:
+            thrust = SMOOTH_TAKEOFF_THRUST
+        set_attitude(thrust = thrust)
+        time.sleep(0.2)
+
+def send_attitude_target(roll_angle = 0.0, pitch_angle = 0.0,
+                         yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,
+                         thrust = 0.5):
+    """
+    use_yaw_rate: the yaw can be controlled using yaw_angle OR yaw_rate.
+                  When one is used, the other is ignored by Ardupilot.
+    thrust: 0 <= thrust <= 1, as a fraction of maximum vertical thrust.
+            Note that as of Copter 3.5, thrust = 0.5 triggers a special case in
+            the code for maintaining current altitude.
+    """
+    if yaw_angle is None:
+        # this value may be unused by the vehicle, depending on use_yaw_rate
+        yaw_angle = vehicle.attitude.yaw
+    # Thrust >  0.5: Ascend
+    # Thrust == 0.5: Hold the altitude
+    # Thrust <  0.5: Descend
+    msg = vehicle.message_factory.set_attitude_target_encode(
+        0, # time_boot_ms
+        1, # Target system
+        1, # Target component
+        0b00000000 if use_yaw_rate else 0b00000100,
+        to_quaternion(roll_angle, pitch_angle, yaw_angle), # Quaternion
+        0, # Body roll rate in radian
+        0, # Body pitch rate in radian
+        math.radians(yaw_rate), # Body yaw rate in radian/second
+        thrust  # Thrust
+    )
+    vehicle.send_mavlink(msg)
+
+def set_attitude(roll_angle = 0.0, pitch_angle = 0.0,
+                 yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,
+                 thrust = 0.5, duration = 0):
+    """
+    Note that from AC3.3 the message should be re-sent more often than every
+    second, as an ATTITUDE_TARGET order has a timeout of 1s.
+    In AC3.2.1 and earlier the specified attitude persists until it is canceled.
+    The code below should work on either version.
+    Sending the message multiple times is the recommended way.
+    """
+    send_attitude_target(roll_angle, pitch_angle,
+                         yaw_angle, yaw_rate, False,
+                         thrust)
+    start = time.time()
+    while time.time() - start < duration:
+        send_attitude_target(roll_angle, pitch_angle,
+                             yaw_angle, yaw_rate, False,
+                             thrust)
+        time.sleep(0.1)
+    # Reset attitude, or it will persist for 1s more due to the timeout
+    send_attitude_target(0, 0,
+                         0, 0, True,
+                         thrust)
+
+def to_quaternion(roll = 0.0, pitch = 0.0, yaw = 0.0):
+    """
+    Convert degrees to quaternions
+    """
+    t0 = math.cos(math.radians(yaw * 0.5))
+    t1 = math.sin(math.radians(yaw * 0.5))
+    t2 = math.cos(math.radians(roll * 0.5))
+    t3 = math.sin(math.radians(roll * 0.5))
+    t4 = math.cos(math.radians(pitch * 0.5))
+    t5 = math.sin(math.radians(pitch * 0.5))
+
+    w = t0 * t2 * t4 + t1 * t3 * t5
+    x = t0 * t3 * t4 - t1 * t2 * t5
+    y = t0 * t2 * t5 + t1 * t3 * t4
+    z = t1 * t2 * t4 - t0 * t3 * t5
+
+    return [w, x, y, z]
+
+# pixy2 Python SWIG get line features example #
+
+get_all_features = True
+
+print("Pixy2 Python SWIG Example -- Get Line Features")
+
+pixy.init()
+pixy.change_prog("line")
+
+class Vector (Structure):
+  _fields_ = [
+    ("m_x0", c_uint),
+    ("m_y0", c_uint),
+    ("m_x1", c_uint),
+    ("m_y1", c_uint),
+    ("m_index", c_uint),
+    ("m_flags", c_uint) ]
+
+class IntersectionLine (Structure):
+  _fields_ = [
+    ("m_index", c_uint),
+    ("m_reserved", c_uint),
+    ("m_angle", c_uint) ]
+
+barcodes = BarcodeArray(100)
+frame = 0
+
+arm()
+guided_nogps()
+# Take off 1m in GUIDED_NOGPS mode.
+takeoff_nogps(1)
+
+no_barcode = True
+
+while no_barcode:
+  set_attitude(duration = 30)
+  if get_all_features:
+    line_get_all_features()
+  else:
+    line_get_main_features()
+  b_count = line_get_barcodes(100, barcodes)
+  if b_count > 0:
+    print('frame %3d:' % (frame))
+    frame = frame + 1
+    no_barcode = False
+    for index in range(0, b_count):
+      print('[BARCODE: X=%d Y=%d CODE=%d]' % (barcodes[index].m_x, barcodes[index].m_y, barcodes[index].m_code))
+
+print("Setting LAND mode...")
+vehicle.mode = VehicleMode("LAND")
+time.sleep(1)
+
+# Close vehicle object before exiting script
+print("Close vehicle object")
+vehicle.close()
+
+# Shut down simulator if it was started.
+if sitl is not None:
+    sitl.stop()
+
+print("Completed")
